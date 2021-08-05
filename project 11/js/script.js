70f218a1f389fcbf93a875106d0162ba78d8c884
ahmedxj <66644016+ahmedxj@users.noreply.github.com>
2021-08-05 08:02:10 +0200

    Add files via upload

--- project 11/js/script.js
+++ project 11/js/script.js
@@ -1,25 +1,27 @@
 
 window.addEventListener('scroll',function(){
     var scroll = document.querySelector('.backtop');
-    scroll.classList.toggle("active" , window.scrollY>500)
-})	
+	var hed = document.querySelector('nav');
+    scroll.classList.toggle("active" , window.scrollY>500),
+	hed.classList.toggle("header1" , window.scrollY>300)
+})		
 
 function scrolltotp(){
 	window.scrollTo({top:0,behavior:'smooth'})
 }
 
-// All animations will take twice the time to accomplish
-document.documentElement.style.setProperty('--animate-duration', '2s');
 
-// All animations will take half the time to accomplish
-document.documentElement.style.setProperty('--animate-duration', '.5s');
 
-;(function ($) {
+var btn = document.querySelector('.hubrger');
+var ulss = document.querySelector('.ulss');
 
-	'use strict'; 
+btn.onclick = function(){
+    "use strict";
 
-$('.content h3 span').counterUp({
-    delay: 10,
-    time: 1000
-});
-})(jQuery);
+    console.log("hello");
+	ulss.classList.toggle("toggle1");
+}
+btn.onblur = function(){
+    "use strict";
+	ulss.classList.toggle("toggle1");
+}
